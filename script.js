9885a3266ed101c82b1d704ade3f8435645dab90
W.I.K.N.Jayathilaka <imalkesara2001@gmail.com>
2023-09-06 16:39:43 +0530

    updated

--- script.js
+++ script.js
@@ -2,34 +2,38 @@ const form = document.querySelector('form');
 let selected = document.querySelector('.selected');
 let hidden = document.querySelector('.hidden');
 
+function letsgo() {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (form.radio.value == 'one') {
+            console.log('one');
+            selected.innerHTML = `You selected 1 out 5`;
+            hidden.classList.remove('hidden');
+            hidden.classList.add('unhidden');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (form.radio.value == 'one') {
-        console.log('one');
-        selected.innerHTML = `You selected 1 out 5`;
-        hidden.classList.remove('hidden');
-        hidden.classList.add('unhidden');
+        } else if (form.radio.value == 'two') {
+            console.log('two');
+            selected.innerHTML = `You selected 2 out 5`;
+            hidden.classList.remove('hidden');
+            hidden.classList.add('unhidden');
+        } else if (form.radio.value == 'three') {
+            console.log('three');
+            selected.innerHTML = `You selected 3 out 5`;
+            hidden.classList.remove('hidden');
+            hidden.classList.add('unhidden');
+        } else if (form.radio.value == 'four') {
+            console.log('four')
+            selected.innerHTML = `You selected 4 out 5`;
+            hidden.classList.remove('hidden');
+            hidden.classList.add('unhidden');
+        } else {
+            console.log('five');
+            selected.innerHTML = `You selected 5 out 5`;
+            hidden.classList.remove('hidden');
+            hidden.classList.add('unhidden');
+        }
+    })
+}
 
-    } else if (form.radio.value == 'two') {
-        console.log('two');
-        selected.innerHTML = `You selected 2 out 5`;
-        hidden.classList.remove('hidden');
-        hidden.classList.add('unhidden');
-    } else if (form.radio.value == 'three') {
-        console.log('three');
-        selected.innerHTML = `You selected 3 out 5`;
-        hidden.classList.remove('hidden');
-        hidden.classList.add('unhidden');
-    } else if (form.radio.value == 'four') {
-        console.log('four')
-        selected.innerHTML = `You selected 4 out 5`;
-        hidden.classList.remove('hidden');
-        hidden.classList.add('unhidden');
-    } else {
-        console.log('five');
-        selected.innerHTML = `You selected 5 out 5`;
-        hidden.classList.remove('hidden');
-        hidden.classList.add('unhidden');
-    }
-})
+
+letsgo();
